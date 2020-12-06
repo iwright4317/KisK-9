8b2788d2c44d5a31cdfe3d29d8a1d0d5b85e6c15
Ian Wright <wrighthouse4317@gmail.com>
2020-12-06 17:07:47 -0500

    new puppy page

---
+++ new_puppy.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>  
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Ki's K-9 Dog Training | Off-leash training</title>
+	<meta name="description" content="Virginia Beach Dog Training. Balanced training; Tips to improve off-leash training">
+	<meta name="author" content="Ki's K-9">
+
+   <!-- Mobile Specific Metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+    ================================================== -->
+   <link rel="stylesheet" href="css/default.css">
+   <link rel="stylesheet" href="css/layout.css">
+   <link rel="stylesheet" href="css/media-queries.css">
+
+   <!-- Script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- Favicons
+	================================================== -->
+	<!--<link rel="shortcut icon" href="favicon.ico" > -->
+	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+
+<?php include 'markup.php';?>
+</head>
+
+<body>
+	
+<?php include 'header.php';?>
+
+   		<div class="row" >
+        	<div class="col-sm-12 " style="text-align: center">        
+   				&nbsp;
+   			</div>
+   		</div>
+      
+      <div class="row" >
+   		
+		<div class="ten columns align-left" style="text-align: left; vertical-align:bottom; color:black; ">
+			<br><h2>Off-leash training/recall</h2>			 
+		</div>	
+		<div class="two columns"  style="text-align: right">	 
+			<img alt="logo Ki's K-9" src="IMG_22221100.jpg" height="60" > 		
+		</div>
+	</div>
+	
+   <!-- Journal Section
+   ================================================== -->
+   <section id="journal">
+
+      <div class="row">
+         <div class="twelve columns align-left">
+         	<h1>Virginia Beach Dog Training</h1>
+<p>
+We are compassionate dog trainers and we provide effective and professional dog training in Virginia Beach and surrounding areas. 
+Why not <a href="contact.php">contact us today</a> for a free consultation?
+<h3>Reliability is the key</h3>
+<p>
+Many dog owners dream of having their dog off-leash and know they have 100% vocal control over their dog. Some dog owners will have their dog off-leash with maybe 70% vocal control over their dog. The problem with this is many dogs will run off, chase a jogger, squirrel, another dog or person, or end up in a very dangerous situation. Dogs off-leash get hit by cars far too often. Many dogs will stay by their owner, but when a distraction presents itself, the dog will take off. This is extremely common and a justified fear among many dog owners. Another fear among dog owners is their dog will take off and not come back. These are all rational, realistic fears. 
+</p>
+<h3>Tools and techniques</h3>
+<p>
+At Ki’s K-9, we like to use the e-collar and positive reinforcement to teach dogs to behave off-leash, when a dog knows that taking off running away or chasing something leads to a consequence, they are far more likely not to do this behavior. That is when the e-collar comes into play as a very important tool to use in dog training off-leash. 
+</p>
+<h3>Rewards are the important</h3>
+<p>
+We always start with a long line and use a very high-quality reward when the dog comes when called. 
+It is extremely important that you never ever call your dog to punish them, this can completely ruin their recall and off-leash progress. We always want our dog to correlate coming back to us as a great reward and whatever that is distracting them is far less important than their owner. 
+</p>
+<h3>Off-leash is rewarding</h3>
+<p>
+When we are able to teach your dog to walk off-leash, many outings become much more enjoyable for your dog and for you. Dogs can get much more exercise when they are able to go off-leash, thus creating a calmer dog inside the house as well. The benefits of having a dog off-leash and know you can trust them are endless. You can go to beaches, hikes, lakes, play fetch in large fields, and just walk around without worrying your dog will run off or get hit by a car. 
+</p>
+<h3>Off-leash does not mean off-manners</h3>
+<p>
+It is extremely important to teach the dog that just because they are off-leash does not mean they can do whatever they want. Sometimes, dogs off-leash like to greet people and dogs who are not dog-friendly. This can end up with an injured dog by mace or other weapon or a dog attack. Once a dog has been attacked there is a much larger problem at hand where we must work on their fearfulness as well as off-leash. 
+</p>
+<h3>Training is very important</h3>
+<p>
+It is important that we teach dogs off-leash the correct way and that their recall is extremely reliable 
+and we follow leash laws. Dogs off-leash are fun but can get into trouble, if you would like to start 
+teaching your dog how to appropriately and correctly behave when off-leash, please contact us or email us 
+at kisk9training@outlook.com or fill out our contact us form 
+</p>
+<p>	
+<b>Videos of off-leash training. </b><br>
+<table border="1"><tr><td><br>
+<iframe  height="440" src="https://www.instagram.com/p/CIRaBEVHLrP/embed" frameborder="0"></iframe>
+</td><td>&nbsp;
+</td>
+</tr></table>
+</p>
+	                     
+</div>
+      </div>
+
+   </section> <!-- Journal Section End-->
+
+
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="footer-nav">
+					<li><a href="#">Home.</a></li>
+              	<li><a href="#">Blog.</a></li>
+              	<li><a href="#">Portfolio.</a></li>
+              	<li><a href="#">About.</a></li>
+              	<li><a href="#">Contact.</a></li>
+               <li><a href="#">Features.</a></li>
+			   </ul>
+
+            <ul class="footer-social">
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+               <li><a href="#"><i class="fa fa-skype"></i></a></li>
+               <li><a href="#"><i class="fa fa-rss"></i></a></li>
+            </ul>
+
+            <!-- <ul class="copyright">
+               <li>Copyright &copy; 2014 Sparrow</li> 
+               <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>               
+            </ul> -->
+
+         </div>
+
+         <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
+
+      </div>
+
+   </footer> <!-- Footer End-->
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/doubletaptogo.js"></script>
+   <script src="js/init.js"></script>
+
+</body>
+
+</html>
