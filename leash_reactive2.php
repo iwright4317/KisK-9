345e8173c8606837bc085f6847fbe9ac795cc9df
poet <iwright4317@gmail.com>
2021-01-14 20:17:02 -0500

    leash reactive 2

---
+++ leash_reactive2.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>  
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Ki's K-9 Dog Training | Leash reactive</title>
+	<meta name="description" content="Virginia Beach Dog Training. Balanced training; Tips to improve leash reactive problems">
+	<meta name="author" content="Ki's K-9">
+
+   <!-- Mobile Specific Metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+    ================================================== -->
+   <link rel="stylesheet" href="css/default.css">
+   <link rel="stylesheet" href="css/layout.css">
+   <link rel="stylesheet" href="css/media-queries.css">
+
+   <!-- Script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- Favicons
+	================================================== -->
+	<!--<link rel="shortcut icon" href="favicon.ico" > -->
+	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+
+<?php include 'markup.php';?>
+</head>
+
+<body>
+	
+<?php include 'header.php';?>
+
+   		<div class="row" >
+        	<div class="col-sm-12 " style="text-align: center">        
+   				&nbsp;
+   			</div>
+   		</div>
+      
+      <div class="row" >
+   		
+		<div class="ten columns align-left" style="text-align: left; vertical-align:bottom; color:black; ">
+			<br><h2>Leash Reactivity </h2>			 
+		</div>	
+		<div class="two columns"  style="text-align: right">	 
+			<img alt="logo Ki's K-9" src="IMG_22221100.jpg" height="60" > 		
+		</div>
+	</div>
+	
+   <!-- Journal Section
+   ================================================== -->
+   <section id="journal">
+
+      <div class="row">
+         <div class="twelve columns align-left">
+         	<h1>Virginia Beach Dog Training</h1>
+<p>For a free consultation, <a href="contact.php">contact us today</a> We are compassionate dog 
+trainers for clients in Virginia Beach and surrounding areas. 
+	
+	<h3>What is Leash Reactivity?</h3>
+	<p>
+When dogs are leash reactive, they can make walks very difficult and honestly quite dangerous. Sometimes when you put a dog on a leash and have a restraint on them, they turn into a different dog. The dog may try to lunge and pull towards people and dogs, barking or showing aggressive or extremely alert body language. Does your dog have their hackles up, stare at other people or dogs? Does your dog have their tail straight up, bark or lunge? Does your dog randomly explode at dogs or people with little to no warning? If so, your dog may be leash reactive. 
+</p>
+<h3>How is Leash Reactivity a problem?</h3>
+	<p>
+This is an extremely common behavioral problem because it is simply unnatural for your dog have on a leash. The restraint can cause them to feel tense; thus becoming reactive. This behavior can be extremely dangerous, especially with large dogs, due to the fact that they are powerful and can knock you over to get to what they want. This can also lead to dog bites on a human or another animal. This can also be very dangerous to owners, going downstairs you can break bones, or have serious injuries. 
+</p>
+<h3>What is my dog thinking?</h3>
+	<p>
+I use an analogy with my clients, I tell them "imagine if a security guard is holding you back, you will feel very tense and probably will become reactive because of the restraint." Because of this unnatural restraint, many dogs do become leash reactive because they want to see that person or dog so badly, that it can become very inappropriate, loud, and dangerous. Or, they will stop paying attention to their owner on walks to try to pull to people and dogs.
+</p>
+<h3>We can help</h3>
+	<p>
+Leash reactivity can be fixed by working with a professional who can show you how to properly counter condition them to these triggers. This behavior is usually fear-based, and thus can be corrected at the root of the problem. Initially, it is best to completely avoid dog to dog meetings on leashes because there is tension in both dogs. 
+</p>
+<h3>This problem should be corrected</h3>
+	<p>
+It’s extremely important that your dog pays attention to you on walks, so leash reactivity is a very 
+serious problem for many dogs. Some dogs are reactive to squirrels, other people, other dogs, cats, trucks, 
+loud noises, or sometimes men. By the term “reactive” we mean the dog behaves very improperly to the 
+trigger by barking, lunging, pulling, or crying. During training, we work extremely hard to get the dog 
+desensitized to these triggers so they can calmly pass these triggers without reacting, lunging, barking, 
+pulling, or crying. We want you to be able to trust your dog on a walk and with proper training, this 
+is attainable. Once the dog realizes the trigger is not more important than paying attention to their 
+owner and it is not that interesting, they will become less reactive and the end goal is for the dog to 
+completely ignore these triggers or passively pass them. If you think your dog may be leash reactive, 
+please contact us for a free consultation. Please watch the video from the link below.</p>
+<p>	
+<b>Videos of Leash Reactive problems. </b><br>
+<table border="1"><tr><td>Asher was a very dog reactive dog 
+<iframe  height="440" src="https://www.instagram.com/p/CIXDJnCHcPd/embed" frameborder="0"></iframe>
+</td><td>Great progress after just one day
+!
+<iframe  height="440" src="https://www.instagram.com/p/CIY83YVHTnK/embed" frameborder="0"></iframe>
+</td>
+</tr></table>
+</p>
+	                     
+</div>
+      </div>
+
+   </section> <!-- Journal Section End-->
+
+
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="footer-nav">
+					<li><a href="#">Home.</a></li>
+              	<li><a href="#">Blog.</a></li>
+              	<li><a href="#">Portfolio.</a></li>
+              	<li><a href="#">About.</a></li>
+              	<li><a href="#">Contact.</a></li>
+               <li><a href="#">Features.</a></li>
+			   </ul>
+
+            <ul class="footer-social">
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+               <li><a href="#"><i class="fa fa-skype"></i></a></li>
+               <li><a href="#"><i class="fa fa-rss"></i></a></li>
+            </ul>
+
+            <!-- <ul class="copyright">
+               <li>Copyright &copy; 2014 Sparrow</li> 
+               <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>               
+            </ul> -->
+
+         </div>
+
+         <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
+
+      </div>
+
+   </footer> <!-- Footer End-->
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/doubletaptogo.js"></script>
+   <script src="js/init.js"></script>
+
+</body>
+
+</html>
